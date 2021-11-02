c433785dab3bfff4cb586f092312c4aa30e7baa0
luyao <luyao@douyu.tv>
2021-11-02 21:27:27 +0800

    【自定义 View 1-1 绘制基础】Pratice7和Pratice8作业提交

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,6 +2,7 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,5 +26,12 @@ public class Practice8DrawArcView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawArc(400, 400, 800, 650, -10, -100, true, paint); //扇形（右上角位）
+        canvas.drawArc(400, 400, 800, 650, 20, 140, false, paint);  // 不封口的弧形（下位）
+        paint.setStyle(Paint.Style.STROKE); // 画线模式
+        canvas.drawArc(400, 400, 800, 650, 180, 60, false, paint); // 不封口的弧形 (左上角位)
     }
 }
