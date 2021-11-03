cb373ddcda314aa6f8e02b9496661c4885da75e4
luyao <luyao@douyu.tv>
2021-11-03 13:06:52 +0800

    【自定义 View 1-1 绘制基础】Pratice10作业提交

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -28,15 +28,48 @@ public class Practice10HistogramView extends View {
         canvas.drawColor(Color.parseColor("#506E7A"));
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+        /**画一横一竖**/
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.WHITE);
         canvas.drawLine(100, 100, 100, 500, paint); // 从上往下画第一笔竖线
         canvas.drawLine(100, 500, 1000, 500, paint); // 第二笔横线
+
+        /**画柱子*/
         paint.setColor(Color.parseColor("#72B916"));
-        canvas.drawRect(120, 496, 220, 500, paint); // 第一个柱子
+        int left = 120;
+        int width = 100;
+        int bottom = 500;
+        canvas.drawRect(left, 496, left+width, bottom, paint); // 第一个柱子
+        left += 130;
+        canvas.drawRect(left, 480, left+width, bottom, paint); // 第二个柱子
+        left += 130;
+        canvas.drawRect(left, 480, left+width, bottom, paint); // 第三个柱子
+        left += 130;
+        canvas.drawRect(left, 280, left+width, bottom, paint); // 第四个柱子
+        left += 130;
+        canvas.drawRect(left, 150, left+width, bottom, paint); // 第五个柱子
+        left += 130;
+        canvas.drawRect(left, 100, left+width, bottom, paint); // 第六个柱子
+        left += 130;
+        canvas.drawRect(left, 290, left+width, bottom, paint); // 第七个柱子
 
-
-//        paint.setColor(Color.WHITE);
-//        paint.setTextSize(40);
-//        canvas.drawText("Froyo", 120, 542, paint);
+        /**画文字*/
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(25);
+        int textLeft = 142;
+        int textMargin = 133;
+        canvas.drawText("Froyo", textLeft, 535, paint);
+        textLeft = textLeft + textMargin;
+        canvas.drawText("GB", textLeft, 535, paint);
+        textLeft = textLeft + textMargin;
+        canvas.drawText("ICS", textLeft, 535, paint);
+        textLeft = textLeft + textMargin;
+        canvas.drawText("JB", textLeft, 535, paint);
+        textLeft = textLeft + textMargin;
+        canvas.drawText("Kitkat", textLeft, 535, paint);
+        textLeft = textLeft + textMargin;
+        canvas.drawText("L", textLeft, 535, paint);
+        textLeft = textLeft + textMargin;
+        canvas.drawText("M", textLeft, 535, paint);
     }
 }
